39654350e1fd5eee9dc9f14db9add1bc36d9aca9
rixshiki <cheunjitnice@gmail.com>
2021-03-03 04:31:52 +0700

    auto send mail can use & add cartconfirm recheck

--- src/main/java/com/example/demo/controllers/GeneralController.java
+++ src/main/java/com/example/demo/controllers/GeneralController.java
@@ -62,6 +62,7 @@ public class GeneralController {
 	
 	@Autowired
 	private SmtpRepository smtpRepo;
+	
 	@GetMapping("/smtp")
 	public String smtp() {
 		return "smtptutorial";
@@ -109,188 +110,11 @@ public class GeneralController {
 		return new ModelAndView("redirect:/myinfo");
 	}
 	
-	@Autowired
-	private OrderDetailRepository orderdetailRepo;
-	@Autowired
-	private ProductDetailRepository productRepo;
 	
-	@GetMapping("/testpage/{msgtype}/{idorder}")
-	public String testpage(@PathVariable("msgtype") Integer msg_type,
-			@PathVariable("idorder") Integer idorder,
-			HttpServletRequest request,
-			Model model) {
-		
-		/* msg_type
-		 * 0 = ผู้ขาย มีรายการคำสั่งซื้อใหม่, ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
-		 * 1 = ผู้ขาย ทำการยกเลิกสินค้า, ผู้ซื้อ ถูกยกเลิกสินค้า
-		 * 2 = ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
-		 * 3 = ผู้ซื้อ ถูกยกเลิกสินค้า
-		
-		String base_url = "http://"+request.getLocalName()+":7070";
-		String target_mail = "rixshiki@gmail.com";
-		String msg_subject, msg_body;
-				
-		msg_subject = mailText(msg_type ,idorder, base_url)[0];
-		msg_body = mailText(msg_type ,idorder, base_url)[1];
-		
-				
-		System.out.println(msg_subject + "\n\n" +msg_body);
-		
-		model.addAttribute("msg_subject", msg_subject);
-		model.addAttribute("msg_body", msg_body);*/
-		
-		//sendEmail(target_mail, msg_subject, msg_body);
+	@GetMapping("/testpage")
+	public String testpage() {
 		
 		return "testpage";
 	}
 	
-	public void toSendMail(Integer msg_type, Integer idorder, String target_mail) {
-		/* msg_type
-		 * 0 = ผู้ขาย มีรายการคำสั่งซื้อใหม่
-		 * 1 = ผู้ขาย ทำการยกเลิกสินค้า
-		 * 2 = ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
-		 * 3 = ผู้ซื้อ ถูกยกเลิกสินค้า*/
-		
-		String base_url = "http://xxx:7070";
-		String msg_subject, msg_body;
-		
-		msg_subject = mailText(msg_type ,idorder, base_url)[0];
-		msg_body = mailText(msg_type ,idorder, base_url)[1];
-		
-		System.out.println(msg_subject +"\n\n"+ msg_body);
-		
-		//sendEmail(target_mail, msg_subject, msg_body);
-	}
-	
-	
-	void sendEmail(String target_mail, String subject, String body_text) {
-		
-		// seller session ..will use from database
-		String username = "rixshiki@gmail.com",
-				password = "fmrftkyefqyonglu";
-		
-		Properties props = new Properties();
-		
-		// set SMTP seller to send G-mail
-		props.put("mail.host", "smtp.gmail.com");
-		props.put("mail.port", "587");
-		props.put("mail.username", username);
-		props.put("mail.password", password);
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.timeout", "5000");
-		props.put("mail.smtp.writetimeout", "5000");
-		props.put("mail.smtp.connectiontimeout", "5000");
-		props.put("mail.smtp.starttls.enable", "true");
-		
-		Session mailSession = Session.getInstance(props,
-			new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(username, password);
-				}
-			});
-		
-		mailSession.setDebug(true);
-		
-		// Send mail
-		MimeMessage msg = new MimeMessage(mailSession);
-
-        try {
-        	// target mail
-			msg.setRecipients(Message.RecipientType.TO, target_mail);
-			msg.setSubject(subject); //subject mail
-			msg.setContent(body_text, "text/html; charset=utf-8"); //text will send to customer
-
-			Transport.send(msg);
-			
-			System.out.println("Success");
-			
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Unsuccess");
-		}
-	}
-	
-	
-	// Message Send
-	String[] mailText(Integer msg_type, Integer idorder, String base_url) {
-		
-		
-		/* msg_type
-		 * 0 = ผู้ขาย มีรายการคำสั่งซื้อใหม่, ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
-		 * 1 = ผู้ขาย ทำการยกเลิกสินค้า, ผู้ซื้อ ถูกยกเลิกสินค้า
-		 * 2 = ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
-		 * 3 = ผู้ซื้อ ถูกยกเลิกสินค้า*/
-		
-		String[] subject_list = {
-				//seller
-				"ท่านได้รับคำสั่งซื้อใหม่ ออเดอร์ #", "ท่านได้ทำการยกเลิกออเดอร์ #",
-				//customer
-				"รายการคำสั่งซื้อของท่าน ออเดอร์ #", "แจ้งเตือนถูกยกเลิกออเดอร์ #"};
-		
-		String[] header_list = {
-				//seller
-				"ออเดอร์ #", "ทำการยกเลิกออเดอร์ #",
-				//customer
-				"ออเดอร์ #", "ผู้ขายได้ทำการยกเลิกออเดอร์ #"};
-		
-		String[] footer_list = {
-				//seller
-				"\n\nราคารวมทั้งสิ้น ",
-				"\n\nต้องคืนเงินรวมทั้งสิ้น ",
-				//customer
-				"\n\nราคารวมทั้งสิ้น ",
-				"\n\nท่านจะได้รับเงินคืนจำนวน "};
-		
-		String[] url_list = {
-				base_url +"/checking",
-				base_url +"/cancel/cancel",
-				base_url +"/buytransfer",
-				base_url
-		};
-		String css =
-				"<style>"
-				+ "table{ width: 500px; border: 1px solid gray; }"
-				+ "table tr:first-child td{ background-color: gray; color: white; }"
-				+ "table td:first-child{ width: 55%; }"
-				+ "table td:NOT(first-child){ width: 15%; }"
-				+ "table tr:last-child td{ background-color: lightgray; color: black; font-weight: bold; }"
-				+ "</style>";
-				
-		List<orderdetail> orderlist = new ArrayList<orderdetail>();
-		orderlist = orderdetailRepo.getByIdorder(idorder);
-		
-		System.out.println(idorder+"list is "+orderlist);
-		
-	    String myorder = "\n<br/><table>"
-	    		+ "<tr><td align='center' colspan='4'>รายการสินค้า</td></tr>";
-	    Integer totalprice = 0;
-	    
-	    // order detail text in mail
-	    for(orderdetail order : orderlist) {
-	    	productdetail product = productRepo.getByIdproduct(order.getIdProduct());
-	    	if(order.getSize() != null) {
-	    		myorder += "\n<tr><td>"+product.getNameProduct()
-	    				+"</td>\n<td>ไซส์ "+order.getSize().toUpperCase()
-	    	    		+"</td>\n<td>x"+order.getNumber()
-	    	    		+"</td>\n<td>"+order.getRealPrice()+" บาท</td>";
-	    		totalprice += order.getRealPrice();
-	    	}else {
-	    		myorder += "\n<tr><td colspan='2'>"+product.getNameProduct()
-	    				+"</td>\n<td>x"+order.getNumber()
-	    				+"</td>\n<td>"+order.getRealPrice()+" บาท</td>";
-	    		totalprice += order.getRealPrice();
-	    	}
-	    	myorder += "\n";
-	    }
-	    
-	    myorder += "\n<tr><td colspan='3'>รวม</td>\n<td>"+ totalprice +".-</td></tr></table>\n<br/>";
-	    myorder += footer_list[msg_type] + totalprice +" บาท\n\n<br/><br/>"+ url_list[msg_type];
-	   	
-		String[] datasend = {
-				subject_list[msg_type] + idorder,
-				css+"<body>"+header_list[msg_type] + idorder + myorder+"</body>"};
-
-		return datasend;
-	}
 }
--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -12,6 +12,15 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -35,6 +44,7 @@ import com.example.demo.Transgroup;
 import com.example.demo.Userjson;
 import com.example.demo.entities.orderdetail;
 import com.example.demo.entities.productdetail;
+import com.example.demo.entities.smtp;
 import com.example.demo.entities.useraddress;
 import com.example.demo.entities.userorder;
 import com.example.demo.entities.userprofile;
@@ -42,6 +52,7 @@ import com.example.demo.repositories.AccountRepository;
 import com.example.demo.repositories.DeliveryRepository;
 import com.example.demo.repositories.OrderDetailRepository;
 import com.example.demo.repositories.ProductDetailRepository;
+import com.example.demo.repositories.SmtpRepository;
 import com.example.demo.repositories.UserAddressRepository;
 import com.example.demo.repositories.UserOrderRepository;
 import com.example.demo.repositories.UserProfileRepository;
@@ -66,6 +77,9 @@ public class OrderController {
 	
 	@Autowired
 	private ProductDetailRepository productRepo;
+	
+	@Autowired
+	private SmtpRepository smtpRepo;
 	
 	@Autowired
 	private CreatePDF pdf;
@@ -438,12 +452,19 @@ public class OrderController {
 	}
 	
 	@GetMapping("/trantocancel/{idorder}")
-	public String trantocancel(@PathVariable("idorder") Integer idorder) {
+	public String trantocancel(@PathVariable("idorder") Integer idorder, HttpServletRequest request) {
 		userorder order = new userorder(); 
 		order = userorderRepo.findById(idorder).get();
 		order.setStatus("cancel");
 		order.setCratedOrder(LocalDateTime.now());
 		userorderRepo.save(order);
+		
+		if(smtpRepo.count() != 0) {
+			String customermail = userprofileRepo.findById(userorderRepo.findById(idorder).get().getIdUser()).get().getEmail(),
+					sellermail = smtpRepo.findAll().get(0).getGmail();
+			toSendMail(1, idorder, sellermail, request.getLocalName());
+			toSendMail(3, idorder, customermail, request.getLocalName());
+		}
 		return "redirect:/tracking";
 	}
 	
@@ -509,7 +530,8 @@ public class OrderController {
 			,@RequestParam(name = "sellbank") String sellbank
 			,@RequestParam(name = "pay") int pay
 			,@RequestParam(name = "paydatetime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime paydatetime
-			,@RequestParam("imageFile") MultipartFile file) throws IOException {
+			,@RequestParam("imageFile") MultipartFile file,
+			HttpServletRequest request) throws IOException {
 		userorder userorder = new userorder();
 		userorder = userorderRepo.findById(sendid).get();
 		userorder.setCratedOrder(LocalDateTime.now());
@@ -524,6 +546,12 @@ public class OrderController {
 			userorder.setPhotoPay(image);
 		}
 		userorderRepo.save(userorder);
+		if(smtpRepo.count() != 0) {
+			String customermail = userprofileRepo.findById(userorderRepo.findById(sendid).get().getIdUser()).get().getEmail(),
+					sellermail = smtpRepo.findAll().get(0).getGmail();
+			toSendMail(0, sendid, sellermail, request.getLocalName());
+			toSendMail(2, sendid, customermail, request.getLocalName());
+		}
 		return "redirect:/buytransfer";
 	}
 
@@ -834,5 +862,164 @@ public class OrderController {
 		List<String> nameaccount = new ArrayList<String>();
 		nameaccount = accountRepo.getAllNamesellaccount();
 		return nameaccount;
+	}
+	
+	
+	
+	/* ======================================== SMTP ======================================== */
+	
+	
+	public void toSendMail(Integer msg_type, Integer idorder, String target_mail, String origin) {
+		/* msg_type
+		 * 0 = ผู้ขาย มีรายการคำสั่งซื้อใหม่
+		 * 1 = ผู้ขาย ทำการยกเลิกสินค้า
+		 * 2 = ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
+		 * 3 = ผู้ซื้อ ถูกยกเลิกสินค้า*/
+		
+		String base_url = "http://" +origin+ ":7070";
+		String msg_subject, msg_body;
+		
+		msg_subject = mailText(msg_type ,idorder, base_url)[0];
+		msg_body = mailText(msg_type ,idorder, base_url)[1];
+		
+		System.out.println(msg_subject +"\n\n"+ msg_body);
+		
+		sendEmail(target_mail, msg_subject, msg_body);
+	}
+	
+	
+	void sendEmail(String target_mail, String subject, String body_text) {
+		
+		// seller session ..will use from database
+		smtp smtp_have = new smtp();
+		smtp_have = smtpRepo.findById(smtpRepo.findAll().get(0).getIdSmtp()).get();
+		
+		String username = smtp_have.getGmail(),
+				password = smtp_have.getPasswordgen(); //fmrftkyefqyonglu
+		
+		
+		Properties props = new Properties();
+		
+		// set SMTP seller to send G-mail
+		props.put("mail.host", "smtp.gmail.com");
+		props.put("mail.port", "587");
+		props.put("mail.username", username);
+		props.put("mail.password", password);
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.timeout", "5000");
+		props.put("mail.smtp.writetimeout", "5000");
+		props.put("mail.smtp.connectiontimeout", "5000");
+		props.put("mail.smtp.starttls.enable", "true");
+		
+		Session mailSession = Session.getInstance(props,
+			new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+		
+		mailSession.setDebug(true);
+		
+		// Send mail
+		MimeMessage msg = new MimeMessage(mailSession);
+
+        try {
+        	// target mail
+			msg.setRecipients(Message.RecipientType.TO, target_mail);
+			msg.setSubject(subject); //subject mail
+			msg.setContent(body_text, "text/html; charset=utf-8"); //text will send to customer
+
+			Transport.send(msg);
+			
+			System.out.println("Success");
+			
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Unsuccess");
+		}
+	}
+	
+	
+	// Message Send
+	String[] mailText(Integer msg_type, Integer idorder, String base_url) {
+		
+		
+		/* msg_type
+		 * 0 = ผู้ขาย มีรายการคำสั่งซื้อใหม่, ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
+		 * 1 = ผู้ขาย ทำการยกเลิกสินค้า, ผู้ซื้อ ถูกยกเลิกสินค้า
+		 * 2 = ผู้ซื้อ แจ้งเตือนรายการคำสั่งซื้อ
+		 * 3 = ผู้ซื้อ ถูกยกเลิกสินค้า*/
+		
+		String[] subject_list = {
+				//seller
+				"ท่านได้รับคำสั่งซื้อใหม่ ออเดอร์ #", "ท่านได้ทำการยกเลิกออเดอร์ #",
+				//customer
+				"รายการคำสั่งซื้อของท่าน ออเดอร์ #", "แจ้งเตือนถูกยกเลิกออเดอร์ #"};
+		
+		String[] header_list = {
+				//seller
+				"ออเดอร์ #", "ทำการยกเลิกออเดอร์ #",
+				//customer
+				"ออเดอร์ #", "ผู้ขายได้ทำการยกเลิกออเดอร์ #"};
+		
+		String[] footer_list = {
+				//seller
+				"\n\nราคารวมทั้งสิ้น ",
+				"\n\nต้องคืนเงินรวมทั้งสิ้น ",
+				//customer
+				"\n\nราคารวมทั้งสิ้น ",
+				"\n\nท่านจะได้รับเงินคืนจำนวน "};
+		
+		String[] url_list = {
+				base_url +"/checking",
+				base_url +"/cancel/cancel",
+				base_url +"/buytransfer",
+				base_url
+		};
+		String css =
+				"<style>"
+				+ "table{ width: 500px; border: 1px solid gray; }"
+				+ "table tr:first-child td{ background-color: gray; color: white; }"
+				+ "table td:first-child{ width: 55%; }"
+				+ "table td:NOT(first-child){ width: 15%; }"
+				+ "table tr:last-child td{ background-color: lightgray; color: black; font-weight: bold; }"
+				+ "</style>";
+				
+		List<orderdetail> orderlist = new ArrayList<orderdetail>();
+		orderlist = orderdetailRepo.getByIdorder(idorder);
+		
+		System.out.println(idorder+"list is "+orderlist);
+		
+	    String myorder = "\n<br/><table>"
+	    		+ "<tr><td align='center' colspan='4'>รายการสินค้า</td></tr>";
+	    Integer totalprice = 0;
+	    
+	    // order detail text in mail
+	    for(orderdetail order : orderlist) {
+	    	productdetail product = productRepo.getByIdproduct(order.getIdProduct());
+	    	if(order.getSize() != null) {
+	    		myorder += "\n<tr><td>"+product.getNameProduct()
+	    				+"</td>\n<td>ไซส์ "+order.getSize().toUpperCase()
+	    	    		+"</td>\n<td>x"+order.getNumber()
+	    	    		+"</td>\n<td>"+order.getRealPrice()+" บาท</td>";
+	    		totalprice += order.getRealPrice();
+	    	}else {
+	    		myorder += "\n<tr><td colspan='2'>"+product.getNameProduct()
+	    				+"</td>\n<td>x"+order.getNumber()
+	    				+"</td>\n<td>"+order.getRealPrice()+" บาท</td>";
+	    		totalprice += order.getRealPrice();
+	    	}
+	    	myorder += "\n";
+	    }
+	    
+	    myorder += "\n<tr><td colspan='3'>รวม</td>\n<td>"+ totalprice +".-</td></tr></table>\n<br/>";
+	    myorder += footer_list[msg_type] + totalprice +" บาท\n\n<br/><br/>"+ url_list[msg_type];
+	   	
+		String[] datasend = {
+				subject_list[msg_type] + idorder,
+				css+"<body>"+header_list[msg_type] + idorder + myorder+"</body>"};
+
+		return datasend;
 	}
 }
