4a3db74a1b709eaa9b72e3d785128c39b3b20ad9
rixshiki <cheunjitnice@gmail.com>
2021-03-07 01:36:52 +0700

    popup forgotpassword

--- src/main/resources/static/JS/general.js
+++ src/main/resources/static/JS/general.js
@@ -3,3 +3,12 @@ $(document).ready(function(){
 	if($('#loading')!="" && $('#loading')!=null){ $('#loading').hide(); }
 	if($('#resultmsg')!="" && $('#resultmsg')!=null){ $('#resultmsg').fadeOut(5000); }
 })
+
+function wait(){
+	$('#loading').show();
+}
+
+function confirmandwait(confirmtxt){
+	if(!confirm(confirmtxt)) return false
+	$('#loading').show();
+}
