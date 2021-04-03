82978d6cdfe55bafc764588c54ddc19b347b32bf
rixshiki <cheunjitnice@gmail.com>
2021-04-03 23:22:42 +0700

    noom check bug

--- src/main/resources/static/JS/general.js
+++ src/main/resources/static/JS/general.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
 	if($('#loading')!="" && $('#loading')!=null) { $('#loading').hide(); }
 	if($('#resultmsg')!="" && $('#resultmsg')!=null) { $('#resultmsg').fadeOut(5000); }
+	
+	
 })
 
 function wait(){
